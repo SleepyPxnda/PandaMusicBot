6448eba8eb8a416c48d89220a7b7f1b6e719063d
Felix <felix@grohme.de>
2020-08-03 15:13:05 +0200

    adjusted functions

--- src/main/java/de/pxnda/Main.java
+++ src/main/java/de/pxnda/Main.java
@@ -2,18 +2,15 @@ package de.pxnda;
 
 import de.pxnda.Logging.ConsoleLogger;
 import de.pxnda.Logging.ILogger;
-import de.pxnda.Utils.SavedSongStorage;
 import de.pxnda.eventhandler.GuildVoiceEventHandler;
 import de.pxnda.eventhandler.MessageEventHandler;
 import de.pxnda.music.PlayerManager;
-import javafx.application.Application;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -29,7 +26,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            jda = new JDABuilder("Njk1MzQ3NTI0ODM1MzQ0NDA0.XpMmlw.ck4FgKN8jYsVaxjiwWTe5iQ-Ewg")
+            jda = new JDABuilder("NzM5MTAwNDQ4NjkwMTQzMjYy.XyVi7g.k2gyw6vBGnBBwph_PnitgzhGM-w")
                     .addEventListeners(new GuildVoiceEventHandler(), new MessageEventHandler())
                     .build();
         } catch (Exception e) {
@@ -37,14 +34,10 @@ public class Main {
         }
         jda.getPresence().setPresence(Activity.listening("to *"), false);
 
-        SavedSongStorage.globalPlaylistStorage.put("finns-dnd", SavedSongStorage.FinnsList);
-
         playerManager = new PlayerManager();
         tempChannelList = new ArrayList<>();
 
         System.out.println("Bot started with prefix " + prefix);
-        System.out.println("Starting Gui ....");
-        
         Logger = new ConsoleLogger();
 
     }
