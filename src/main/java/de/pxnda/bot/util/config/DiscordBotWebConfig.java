657525347c02a2b062d3a0ee15ac808c220276f0
Felix <nope>
2021-01-17 21:01:46 +0100

    refactored to custom weblogger

--- src/main/java/de/pxnda/bot/util/config/DiscordBotWebConfig.java
+++ src/main/java/de/pxnda/bot/util/config/DiscordBotWebConfig.java
@@ -1,5 +1,6 @@
 package de.pxnda.bot.util.config;
 
+import de.pxnda.bot.rest.CustomWebRequestLogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
@@ -9,12 +10,7 @@ public class DiscordBotWebConfig {
 
     @Bean
     public CommonsRequestLoggingFilter requestLoggingFilter() {
-        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
-        filter.setIncludeQueryString(true);
-        filter.setIncludePayload(true);
-        filter.setMaxPayloadLength(10000);
-        filter.setIncludeHeaders(false);
-        return filter;
+        return new CustomWebRequestLogger();
     }
 
 }
